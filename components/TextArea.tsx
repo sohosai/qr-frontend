375e0258e9b1749b1ddd74f19bd8d8fb7c63665b
iori0705 <iodere77@gmail.com>
2023-02-08 22:30:46 +0900

    📝 入力欄を作成

--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -1,7 +1,16 @@
+import { useState, useMemo } from "react"
 export default function TextArea () {
+  const [fixturesDescription, setFixturesDescription] = useState("")
+  const onChangeFixturesDescription = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    setFixturesDescription(event.target.value)
+  }
   return (
     <div>
-     
-    </div>
+      <label>label</label>
+      <textarea
+        value={fixturesDescription}
+        onChange={onChangeFixturesDescription}
+      ></textarea>
+  </div>
   );
 }
