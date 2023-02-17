ebf67c0647b039f1478d5963e98a50073d5c7d0f
iori0705 <iodere77@gmail.com>
2023-02-18 06:53:05 +0900

    📝 inputからtextareaに変更 labelとtextareaにCSSを追加

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,21 +1,31 @@
 import React from "react"
+import styled from "styled-components"
 
+//TextProps型の作成
 type TextProps = {
-  label?: string
+  label: string
   text: string
   placeholder: string
   onChange: (value: string) => void
 }
 
 export const TextArea = ({ label, text, placeholder, onChange }: TextProps) => {
+  //StyledLabelにCSSを適用したlabel要素を適用
+  const StyledLabel = styled.label`
+    color: #555555
+  `
+  //StyledTextareaにCSSを適用したtextarea要素を適用
+  const StyledTextarea = styled.textarea`
+    ::placeholder: #C5C5C5
+  `
   return (
-    <label style={{ display: "block" }}>
+    <StyledLabel style={{ display: "block" }}>
       { label }
-      <input
+      <StyledTextarea
         value = {text}
-        onChange = {event => onChange(event.target.value)}
+        onChange = {(event: React.ChangeEvent<HTMLInputElement>) => onChange(event.target.value)}
         placeholder = {placeholder}
         />
-    </label>
+    </StyledLabel>
   );
 }
