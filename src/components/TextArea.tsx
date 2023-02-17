497d5d90892713fdb736dfac29c599fb971c82f3
iori0705 <iodere77@gmail.com>
2023-02-18 07:17:44 +0900

    📝 デザインを修正

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -12,20 +12,34 @@ type TextProps = {
 export const TextArea = ({ label, text, placeholder, onChange }: TextProps) => {
   //StyledLabelにCSSを適用したlabel要素を適用
   const StyledLabel = styled.label`
-    color: #555555
+    color: #555555;
+    font-family: "Inter", "sans-serif";
+    font-size: 8px;
   `
   //StyledTextareaにCSSを適用したtextarea要素を適用
   const StyledTextarea = styled.textarea`
+    font-family: "Inter", "sans-serif";
+    font-size: 12px;
+    border-radius: 5px;
     ::placeholder: #C5C5C5
   `
   return (
-    <StyledLabel style={{ display: "block" }}>
-      { label }
+    <div>
+      <StyledLabel
+      style = {{ display: "block" }}
+      for = "textarea"
+      >
+        { label }
+      </StyledLabel>
+
       <StyledTextarea
+        id = "textarea"
+        rows = "4"
+        cols = "33"
         value = {text}
         onChange = {(event: React.ChangeEvent<HTMLInputElement>) => onChange(event.target.value)}
         placeholder = {placeholder}
-        />
-    </StyledLabel>
+      />
+    </div>
   );
 }
