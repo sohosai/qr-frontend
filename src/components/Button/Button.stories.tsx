887304dffd59f2e61503d9f94377504deaceb01f
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2023-02-21 15:09:48 +0900

    Storybookの導入とそれに伴うディレクトリの変更

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import Button from "."
+
+export default {
+    title: 'Components/Button',
+    component: Button
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+export const Common = Template.bind({})
+Common.args = {
+    disabled: true,
+    text: "登録",
+    onClick: () => {},
+}
