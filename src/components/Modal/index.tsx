04cf3f64510a514bacbdb1df916c2de4c26ac63d
iori0705 <iodere77@gmail.com>
2023-02-24 19:36:26 +0900

    📝 ModalコンポーネントにStyleを適用

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,14 +1,33 @@
 import ReactModal from "react-modal";
+import styled, { CSSProperties } from "styled-components";
 
 type ModalProps = {
     show: boolean;
     children: React.ReactNode;
 }
 
+type TypeModalStyle = {
+    overlay: React.CSSProperties;
+    content: React.CSSProperties;
+}
+
+const StyleReactModal: TypeModalStyle = {
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.50)'
+    },
+    content: {
+        position: "absolute",
+        margin:  "0 auto",
+        width: "320px",
+        height: "370px",
+        background: '#E5E5E5',
+    }
+};
+
 const Modal = ({ show, children }: ModalProps) => {
     return (
         <div>
-            <ReactModal isOpen={show}>{children}</ReactModal>
+            <ReactModal isOpen={show} style={StyleReactModal} >{children}</ReactModal>
         </div>
     );
 };
