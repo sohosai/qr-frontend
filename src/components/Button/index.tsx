01adab0b07db2041ee1cbcc37c3bce29f6b8112c
momeemt <momiimt@gmail.com>
2023-03-14 11:43:08 +0900

    ボタンのborder-raduisを修正

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,8 +10,14 @@ export default function Button ({ disabled, text, onClick }: ButtonProp ) {
     let bgColorClassName = disabled ? "bg-neutral-300" : "bg-white"
     let textColorClassName = disabled ? "text-neutral-500" : "text-orange-600" 
     return(
-        <button className={`w-24 h-8 rounded-lg ${bgColorClassName}`} disabled={disabled} onClick={onClick}>
-            <span className={`font-bold center ${textColorClassName}`}>{text}</span>
+        <button
+            className={`w-24 h-8 rounded-xl ${bgColorClassName}`}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            <span className={`font-bold center ${textColorClassName}`}>
+                {text}
+            </span>
         </button>
     );
 }
