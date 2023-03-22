518040edaa431e91c84fae0514a43d5d482cdbb6
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2023-03-22 15:25:38 +0900

    セレクトボックスにラベルを追加

---
+++ src/components/SelectBox/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+interface Props {
+  label: string;
+  options: string[]; // 外部から渡される選択肢
+}
+
+interface State {
+  selectedOption: string | null; // 選択された選択肢
+}
+
+class SelectBox extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      selectedOption: null,
+    };
+  }
+
+  handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedOption = event.target.value;
+    this.setState({ selectedOption });
+  };
+
+  render() {
+    const { label } = this.props;
+    const { options } = this.props;
+    const { selectedOption } = this.state;
+  
+    return (
+      <><label>{label}</label>
+        <select value={selectedOption ? selectedOption : ''} onChange={this.handleOptionChange}>
+        <option value="">選択してください</option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+        </select>
+      </>
+    );
+  }
+  
+}
+
+export default SelectBox;
