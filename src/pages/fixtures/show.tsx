45089e3cdb4423d0eb560141e373dfa73d5549f2
puripuri2100 <puripuri2100@gmail.com>
2023-08-28 12:57:52 +0900

    とりあえず個別ページ作成 デザインは壊滅なのでリファクタリングよろしく

--- src/pages/fixtures/show.tsx
+++ src/pages/fixtures/show.tsx
@@ -1,16 +1,33 @@
-import { Fixtures, QrColor, Stroge } from "../../types";
-import { initQRCode } from "@/lib/QRCode";
-import QRCode from "../../components/QRCode";
+import { Fixtures } from '../../types'
+import { initQRCode, QrColor2QRCodeColor } from '@/lib/QRCode'
+import QRCode from '../../components/QRCode'
 
-const FixturesShow = (fixtures: Fixtures) => {
-  return (<>
-    <h1>{fixtures.name}</h1>
-    <p>uuid: {fixtures.id}</p>
-    <p>description</p>
-    {fixtures.description?.split('\n').map(s => <p>{s}</p>)}
-    <p>note: </p>
-    {fixtures.note?.split('\n').map(s => <p>{s}</p>)}
-  </>)
+type FixturesProps = {
+  fixtures: Fixtures
+}
+
+const FixturesShow = ({ fixtures }: FixturesProps) => {
+  return (
+    <>
+      <h1>{fixtures.name}</h1>
+      <p>{fixtures.model_number}</p>
+      <p>uuid: {fixtures.id}</p>
+      <QRCode qr={initQRCode(fixtures.qr_id, QrColor2QRCodeColor(fixtures.qr_color))}></QRCode>
+      <p>
+        保管場所：{fixtures.storage}/{fixtures.parent_id}
+      </p>
+      <p>description</p>
+      {fixtures.description?.split('\n').map((s) => (
+        <p>{s}</p>
+      ))}
+      <p>note: </p>
+      {fixtures.note?.split('\n').map((s) => (
+        <p>{s}</p>
+      ))}
+      <p>用途：{fixtures.usage}</p>
+      <p>使用時期：{fixtures.usage_season}</p>
+    </>
+  )
 }
 
 export default FixturesShow
