c183af583e0d20f328e9e487872ad5d4f98c5744
puripuri2100 <puripuri2100@gmail.com>
2023-08-29 16:48:28 +0900

    bug fix

--- src/pages/fixtures/show.tsx
+++ src/pages/fixtures/show.tsx
@@ -7,27 +7,29 @@ type FixturesProps = {
 }
 
 const FixturesShow = ({ fixtures }: FixturesProps) => {
-  return (
-    <>
-      <h1>{fixtures.name}</h1>
-      <p>{fixtures.model_number}</p>
-      <p>uuid: {fixtures.id}</p>
-      <QRCode qr={initQRCode(fixtures.qr_id, QrColor2QRCodeColor(fixtures.qr_color))}></QRCode>
-      <p>
-        保管場所：{fixtures.storage}/{fixtures.parent_id}
-      </p>
-      <p>description</p>
-      {fixtures.description?.split('\n').map((s) => (
-        <p key={fixtures.id + "description" + s}>{s}</p>
-      ))}
-      <p>note: </p>
-      {fixtures.note?.split('\n').map((s) => (
-        <p key={fixtures.id + "note" + s}>{s}</p>
-      ))}
-      <p>用途：{fixtures.usage}</p>
-      <p>使用時期：{fixtures.usage_season}</p>
-    </>
-  )
+  if (typeof fixtures !== 'undefined') {
+    return (
+      <>
+        <p>{fixtures.name}</p>
+        {fixtures.model_number !== null ? <p>{fixtures.model_number}</p> : <></>}
+        <p>uuid: {fixtures.id}</p>
+        <QRCode qr={initQRCode(fixtures.qr_id, QrColor2QRCodeColor(fixtures.qr_color))}></QRCode>
+        <p>
+          保管場所：{fixtures.storage}/{fixtures.parent_id}
+        </p>
+        <p>description</p>
+        {fixtures.description?.split('\n').map((s) => (
+          <p key={fixtures.id + "description" + s}>{s}</p>
+        ))}
+        <p>note: </p>
+        {fixtures.note?.split('\n').map((s) => (
+          <p key={fixtures.id + "note" + s}>{s}</p>
+        ))}
+        {fixtures.usage !== null ? <p>用途：{fixtures.usage}</p> : <></>}
+        {fixtures.usage_season !== null ? <p>使用時期：{fixtures.usage_season}</p> : <></>}
+      </>
+    )
+  }
 }
 
 export default FixturesShow
