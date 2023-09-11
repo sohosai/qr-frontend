d032aa2a1f8300551a65fd7f3a77ca7722b7fc9b
puripuri2100 <puripuri2100@gmail.com>
2023-09-11 21:39:51 +0900

    備品ページをAPIをたたく方法に更新 たぶん動く

--- src/pages/items/[fixtures_id].tsx
+++ src/pages/items/[fixtures_id].tsx
@@ -1,54 +1,79 @@
+import { useEffect, useState } from 'react'
 import { Fixtures } from '@/types'
 import { useRouter } from 'next/router'
 import { initQRCode } from '@/lib/QRCode'
 import QRCode from '@/components/QRCode'
 import Header from '@/components/Header'
+import axios from 'axios'
 
 const FixturesShow = () => {
   const route = useRouter()
-  const fixtures_id = route.query.fixtures_id
-  if (typeof fixtures_id == 'string') {
-    // ダミー値
-    // 本来はバックエンドのAPIを叩いて取得する
-    const now = new Date()
-    const fixtures: Fixtures = {
-      id: '550e8400-e29b-41d4-a716-446655440000',
-      created_at: now,
-      qr_id: fixtures_id,
-      qr_color: 'red',
-      name: 'test',
-      description: 'test用のダミー',
-      note: '',
-      parent_id: 'xxxx',
-      model_number: null,
-      storage: 'room101',
-      usage: null,
-      usage_season: null,
+  const [fixtures, setFixtures] = useState<Fixtures | null>(null)
+  const [isOk, setIsOk] = useState(false)
+
+  useEffect(() => {
+    if (typeof route.query.fixtures_id == 'string') {
+      const fixtures_id = route.query.fixtures_id
+      const api_url = process.env.NEXT_PUBLIC_QR_API_URL
+      console.log(api_url)
+      if (fixtures_id !== null && api_url !== undefined) {
+        ;(async () => {
+          const url = api_url + '/get_fixtures?qr_id=' + fixtures_id
+          console.log('url:')
+          console.log(url)
+          setIsOk(true)
+          try {
+            const response = await axios.get(url)
+            setFixtures(response.data.results)
+            return response
+          } catch (err) {
+            setFixtures(null)
+          }
+        })()
+      } else {
+        setIsOk(false)
+      }
     }
+  }, [route])
+
+  if (isOk) {
     return (
       <>
         <Header />
-        <p>{fixtures.name}</p>
-        {fixtures.model_number !== null ? <p>{fixtures.model_number}</p> : <></>}
-        <p>uuid: {fixtures.id}</p>
-        <QRCode qr={initQRCode(fixtures.qr_id, fixtures.qr_color)}></QRCode>
-        <p>
-          保管場所：{fixtures.storage}/{fixtures.parent_id}
-        </p>
-        <p>description</p>
-        {fixtures.description?.split('\n').map((s) => (
-          <p key={fixtures.id + 'description' + s}>{s}</p>
-        ))}
-        <p>note: </p>
-        {fixtures.note?.split('\n').map((s) => (
-          <p key={fixtures.id + 'note' + s}>{s}</p>
-        ))}
-        {fixtures.usage !== null ? <p>用途：{fixtures.usage}</p> : <></>}
-        {fixtures.usage_season !== null ? <p>使用時期：{fixtures.usage_season}</p> : <></>}
+        {fixtures !== null ? (
+          <>
+            <p>{fixtures.name}</p>
+            {fixtures.model_number !== null ? <p>{fixtures.model_number}</p> : <></>}
+            <p>uuid: {fixtures.id}</p>
+            <QRCode qr={initQRCode(fixtures.qr_id, fixtures.qr_color)}></QRCode>
+            <p>
+              保管場所：{fixtures.storage}/{fixtures.parent_id}
+            </p>
+            <p>description</p>
+            {fixtures.description?.split('\n').map((s) => (
+              <p key={fixtures.id + 'description' + s}>{s}</p>
+            ))}
+            <p>note: </p>
+            {fixtures.note?.split('\n').map((s) => (
+              <p key={fixtures.id + 'note' + s}>{s}</p>
+            ))}
+            {fixtures.usage !== null ? <p>用途：{fixtures.usage}</p> : <></>}
+            {fixtures.usage_season !== null ? <p>使用時期：{fixtures.usage_season}</p> : <></>}
+          </>
+        ) : (
+          <>
+            <p>適切なIDではありません</p>
+          </>
+        )}
       </>
     )
   } else {
-    return <></>
+    return (
+      <>
+        <Header />
+        <p>API URLが無い、もしくはURLが無効</p>
+      </>
+    )
   }
 }
 
