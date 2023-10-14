c9b11f3684a11fb1d205df805c8cbe7a99e00e66
puripuri2100 <puripuri2100@gmail.com>
2023-10-15 00:06:31 +0900

    トップページにQRコード読み取りボタンを配置し、ページに飛ぶようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
+import React, { useState, useEffect } from 'react'
 import Header from '@/components/Header'
+import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner'
+import IconButton from '@mui/material/IconButton'
+import QrCodeReader from '@/components/QRCodeReader'
+import { useRouter } from 'next/router'
 
 /**
  * QRにログインした際に最初に表示されるトップページ
  * 各ページに飛べるようにボタンを配置する
  */
 export default function Home() {
+  const router = useRouter()
+  const [qrId, setQrId] = useState('')
+  const onChangeQrId = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setQrId(event.target.value)
+  }
+  const [isOpenQrReader, setIsOpenQrReader] = useState(false)
+
+  useEffect(() => {
+    if (qrId !== "") {
+      router.replace(`/items/${qrId}`)
+    }
+  }, [qrId])
+
   return (
     <>
       <Header />
@@ -17,6 +35,35 @@ export default function Home() {
       >
         物品管理アプリケーション
       </div>
+      {isOpenQrReader ? (
+        <QrCodeReader
+          onReadCode={(url) => {
+            // urlは"https://qr.sohosai.com/items/XWPV"のような形をしている
+            const str_lst = url.split('/')
+            const id = str_lst.pop()
+            if (id) {
+              setQrId(id)
+            }
+          }}
+        />
+      ) : (
+        <></>
+      )}
+      <IconButton
+        size='large'
+        background-color='#6600CC'
+        sx={{
+          color: '#6600CC',
+          border: '1px solid #6600CC',
+          boxShadow: '1px 1px 5px 1px  #998fa3',
+          textAlign: 'cetner'
+        }}
+        onClick={() => {
+          setIsOpenQrReader(!isOpenQrReader)
+        }}
+      >
+        <QrCodeScannerIcon fontSize='inherit' />
+      </IconButton>
     </>
   )
 }
